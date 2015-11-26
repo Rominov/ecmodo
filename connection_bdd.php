2172ba442714a926cea0dd47a60b4ca84f964094
Rominov <rominov@rominov.fr>
2015-11-26 15:25:58 +0100

    Login mysql
    Mise à jour du mot de passe root

--- connection_bdd.php
+++ connection_bdd.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=127.0.0.1;dbname=ecmodov3', 'root', '');
+	$bdd = new PDO('mysql:host=127.0.0.1;dbname=ecmodov3', 'root', '7ip94b16');
 }
 catch(Exception $e)
 {
