839269a8a65e8b573ff9a7e72ab0344faa1e86bf
Rominov <rominov@rominov.fr>
2015-11-26 15:28:28 +0100

    màj du footer 3.0 béta

--- foo.php
+++ foo.php
@@ -2,7 +2,7 @@
 <div class="footer">
 	<div class="footer-inner">
 		<div class="container">
-			<br> v2.0 © 2015. Tous droits réservés Rominov, Elaya, GaMa(CSS), Yukii.
+			<br> v3.0 Beta © 2015. Tous droits réservés Rominov, Elaya, GaMa(CSS), Yukii.
 		</div> <!-- /container -->
 	</div> <!-- /footer-inner -->
 </div> <!-- /footer -->	
