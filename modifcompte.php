5077b832117b694b13808ea7d656ed9b10ae7d8d
ArkYukii <mathieu.deberranger@gmail.com>
2015-11-26 15:38:17 +0100

    v3.0

--- modifcompte.php
+++ modifcompte.php
@@ -7,7 +7,7 @@ $skype = $_POST['skype'];
 
 if($mail != "" && $skype != "" && $pwd != "")
 {
-	$sql = "UPDATE modo SET pass='".$pwd."', skype='".$skype."', mail='".$mail."' WHERE nom='".$_COOKIE['connexion']."'";
+	$sql = "UPDATE modo SET pass='".md5($pwd)."', skype='".$skype."', mail='".$mail."' WHERE nom='".$_COOKIE['connexion']."'";
 	$req = $bdd->prepare($sql);
 	$req->execute();
 }
@@ -19,13 +19,13 @@ else if($mail != "" && $skype != "")
 }
 else if ($mail != "" && $pwd != "") 
 {
-	$sql = "UPDATE modo SET mail='".$mail."', pass='".$pwd."' WHERE nom='".$_COOKIE['connexion']."'";
+	$sql = "UPDATE modo SET mail='".$mail."', pass='".md5($pwd)."' WHERE nom='".$_COOKIE['connexion']."'";
 	$req = $bdd->prepare($sql);
 	$req->execute();
 }
 else if ($skype != "" && $pwd != "")
 {
-	$sql = "UPDATE modo SET pass='".$pwd."', skype='".$skype."' WHERE nom='".$_COOKIE['connexion']."'";
+	$sql = "UPDATE modo SET pass='".md5($pwd)."', skype='".$skype."' WHERE nom='".$_COOKIE['connexion']."'";
 	$req = $bdd->prepare($sql);
 	$req->execute();
 }
@@ -38,7 +38,7 @@ else if($skype != "")
 }
 else if($pwd != "")
 {
-	$sql = "UPDATE modo SET pass='".$pwd."' WHERE nom='".$_COOKIE['connexion']."'";
+	$sql = "UPDATE modo SET pass='".md5($pwd)."' WHERE nom='".$_COOKIE['connexion']."'";
 	$req = $bdd->prepare($sql);
 	$req->execute();
 }
